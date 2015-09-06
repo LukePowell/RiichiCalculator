2ad37953c55cb12940eeb55298e761b34dd6a92b
Luke Powell <luke.powell@dragonwellstudios.com>
2015-09-06 16:38:06 -0700

    Copyright notice

--- Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/Yaku.java
+++ Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/Yaku.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2015.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 package com.dragonwellstudios.mahjonghandhelper.riichi;
 
 import com.dragonwellstudios.mahjonghandhelper.Tile;
@@ -6,6 +25,7 @@ import com.dragonwellstudios.mahjonghandhelper.Tile;
  * Created by Luke Powell on 9/6/2015.
  *
  * Stores all the necessary information about a yaku to be displayed
+ *
  */
 public class Yaku {
     //region FIELDS --------------------------------------------------------------------------------

727a1208a93245b6a6e4d3345c7bf9852f5e771b
luke.powell <luke.powell@gmail.com>
2015-10-05 10:35:37 -0700

    Base points calculation for all han/fu pairings even if they are not possible to achieve in game.

--- Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/ScoreTable.java
+++ Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/ScoreTable.java
@@ -14,14 +14,53 @@ public class ScoreTable {
         this.ruleSet = ruleSet;
     }
 
+    //region CONSTANTS
+    public final int MANGAN_BASE_POINTS = 2000;
+    public final int HANEMAN_BASE_POINTS = 3000;
+    public final int BAIMAN_BASE_POINTS = 4000;
+    public final int SANBAIMAN_BASE_POINTS = 6000;
+    public final int YAKUMAN_BASE_POINTS = 8000;
+    //endregion
+
+    /**
+     * Calculates the base points for the given han and fu, this assumes that fu has already been
+     * rounded to the correct value.
+     * @param han The han count
+     * @param fu The fu count
+     * @return base points for the given han and fu
+     */
+    public int getBasePoints(int han, int fu){
+        switch(han){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                return fu * (int)Math.pow(2,2+han);
+            case 5:
+                return MANGAN_BASE_POINTS;
+            case 6:
+            case 7:
+                return HANEMAN_BASE_POINTS;
+            case 8:
+            case 9:
+            case 10:
+                return BAIMAN_BASE_POINTS;
+            case 11:
+            case 12:
+                return SANBAIMAN_BASE_POINTS;
+            default://Handled all valid values < Yakuman, thus default to yakuman
+                return YAKUMAN_BASE_POINTS;
+        }
+    }
+
     /**
      * Calculates the final score from the given han and fu values
      * @param han The count of han in the hand
      * @param fu The count of fu in the given hand
      * @return The final calculated score accounting for any rule set variations
      */
-     public int getFinalScore(int han, int fu, boolean dealer){
+     public Payout getPayout(int han, int fu, boolean dealer, boolean ron){
         //Determine the final score based on any current rule sets
-        return -1;
+        return null;
     }
 }

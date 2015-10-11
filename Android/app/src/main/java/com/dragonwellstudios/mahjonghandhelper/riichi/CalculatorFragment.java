deadfe8d1348d75a094bb18f6549b735216f5f0e
luke.powell <luke.powell@gmail.com>
2015-10-11 00:09:58 -0700

    Moved values for fu/han picker to be stored in code. Created view model for the calculator

--- Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/CalculatorFragment.java
+++ Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/CalculatorFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 import com.dragonwellstudios.mahjonghandhelper.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Luke Powell on 10/7/15.
@@ -15,6 +19,39 @@ import com.dragonwellstudios.mahjonghandhelper.R;
  * Simple fragment to display a dialog using calculator_dialog.xml
  */
 public class CalculatorFragment extends DialogFragment {
+
+    /**
+     * Valid values for {@see R.id.hanPicker}
+     */
+    private static final String[] hanValues = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13+"};
+    /**
+     * Valid values for {@see R.id.fuPicker}
+     */
+    private static final String[] fuValues = {"20", "25", "30", "40", "50", "60", "70", "80", "90", "100", "110"};
+    //endregion
+
+
+    //region CONSTANTS
+    //region VIEWS
+    @Bind(R.id.hanNumberPicker)
+    NumberPicker hanPicker;
+    @Bind(R.id.fuNumberPicker)
+    NumberPicker fuPicker;
+    //endregion
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ButterKnife.bind(this, getView());
+
+        hanPicker.setMaxValue(hanValues.length - 1);
+        hanPicker.setDisplayedValues(hanValues);
+
+        fuPicker.setMaxValue(fuValues.length - 1);
+        fuPicker.setDisplayedValues(fuValues);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
---
+++ Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/CalculatorViewModel.java
@@ -0,0 +1,17 @@
+package com.dragonwellstudios.mahjonghandhelper.riichi;
+
+/**
+ * Created by Luke Powell on 10/10/15.
+ */
+public class CalculatorViewModel {
+
+    private final ScoreTable table;
+    private int han = 1;
+    private int fu = 20;
+
+    public CalculatorViewModel(ScoreTable table) {
+        this.table = table;
+    }
+
+
+}

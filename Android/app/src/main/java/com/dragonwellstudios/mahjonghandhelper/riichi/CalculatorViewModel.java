0b613d69dba696085f010d7aab58e5c4003f222e
luke.powell <luke.powell@gmail.com>
2015-10-11 00:19:24 -0700

    Moved values for fu/han picker to be stored in code. Created view model for the calculator

--- Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/CalculatorViewModel.java
+++ Android/app/src/main/java/com/dragonwellstudios/mahjonghandhelper/riichi/CalculatorViewModel.java
@@ -1,17 +1,49 @@
 package com.dragonwellstudios.mahjonghandhelper.riichi;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.dragonwellstudios.mahjonghandhelper.BR;
+
 /**
  * Created by Luke Powell on 10/10/15.
+ *
+ * View model used in calculator fragment
  */
-public class CalculatorViewModel {
+public class CalculatorViewModel extends BaseObservable {
 
     private final ScoreTable table;
     private int han = 1;
     private int fu = 20;
+    private Payout payout;
+    private String payoutFormatted;
 
     public CalculatorViewModel(ScoreTable table) {
         this.table = table;
     }
 
+    @Bindable
+    public int getHan() {
+        return han;
+    }
 
+    public void setHan(int value) {
+        han = value;
+        notifyPropertyChanged(BR.han);
+    }
+
+    @Bindable
+    public int getFu() {
+        return fu;
+    }
+
+    public void setFu(int value) {
+        fu = value;
+        notifyPropertyChanged(BR.fu);
+    }
+
+    @Bindable
+    public String getPayoutFormatted() {
+        return payoutFormatted;
+    }
 }
